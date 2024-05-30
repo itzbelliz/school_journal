c77de02b2b8ad49c23a4c32d5ad936e47f35f6bc
GruchaLapekDom <gruchadebian@gmail.com>
2024-05-30 18:19:42 +0200

    prepare login to connect database

--- login.php
+++ login.php
@@ -30,13 +30,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 </head>
 <body>
     <form action="login.php" method="post">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required>
+        <label for="login"></label>
+        <input type="text" id="login" name="login" required>
         
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" required>
         
-        <button type="submit">Login</button>
+        <button type="submit">Zaloguj</button>
     </form>
 </body>
 </html>
